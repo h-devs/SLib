353c5de6e6edf613e922e8d0eea69a724f526e10
Fred Zhang <fred1218zh@outlook.com>
2020-06-22 15:16:35 +0900

    [Win32] media/AudioPlayer: improve DirectSound module

--- src/slib/media/audio_player_dsound.cpp
+++ src/slib/media/audio_player_dsound.cpp
@@ -45,10 +45,124 @@ namespace slib
 		namespace dsound
 		{
 
+			class AudioPlayerImpl : public AudioPlayer
+			{
+			public:
+				LPDIRECTSOUND m_ds;
+				GUID m_deviceID;
+
+			public:
+				AudioPlayerImpl()
+				{
+					m_ds = NULL;
+				}
+
+				~AudioPlayerImpl()
+				{
+					release();
+				}
+
+			public:
+				static void logError(String text)
+				{
+					LogError("AudioPlayer", text);
+				}
+
+				static Ref<AudioPlayerImpl> create(const AudioPlayerParam& param)
+				{
+					CoInitializeEx(NULL, COINIT_MULTITHREADED);
+					
+					GUID gid;
+					String deviceID = param.deviceId;
+					if (deviceID.isEmpty()) {
+						gid = DSDEVID_DefaultPlayback;
+					} else {
+						ListLocker<DeviceProperty> props(queryDeviceInfos());
+						sl_size i = 0;
+						for (; i < props.count; i++) {
+							if (deviceID == props[i].szGuid) {
+								gid = props[i].guid;
+								break;
+							}
+						}
+						if (i == props.count) {
+							logError("Failed to find player device");
+							return sl_null;
+						}
+					}
+
+					LPDIRECTSOUND ds;
+					HRESULT hr = DirectSoundCreate(&gid, &ds, NULL);
+					if (SUCCEEDED(hr)) {
+						hr = ds->SetCooperativeLevel(GetDesktopWindow(), DSSCL_NORMAL);
+						if (SUCCEEDED(hr)) {
+							Ref<AudioPlayerImpl> ret = new AudioPlayerImpl();
+							if (ret.isNotNull()) {
+								ret->m_deviceID = gid;
+								ret->m_ds = ds;
+								return ret;
+							}
+						} else {
+							logError("Direct sound set CooperativeLevel failed.");
+						}
+						ds->Release();
+					} else {
+						if (hr == DSERR_ALLOCATED) {
+							logError("Direct sound playback device is already used");
+						} else {
+							logError("Can not create direct sound playback device");
+						}
+					}
+					return sl_null;
+				}
+
+				void release()
+				{
+					ObjectLocker lock(this);
+					if (m_ds) {
+						m_ds->Release();
+						m_ds = NULL;
+					}
+				}
+
+				Ref<AudioPlayerBuffer> createBuffer(const AudioPlayerBufferParam& param) override;
+
+				struct DeviceProperty {
+					GUID guid;
+					String szGuid;
+					String name;
+					String description;
+				};
+
+				static List<DeviceProperty> queryDeviceInfos()
+				{
+					List<DeviceProperty> list;
+					HRESULT hr = DirectSoundEnumerateW(DeviceEnumProc, (VOID*)&list);
+					if (FAILED(hr)) {
+						logError("Can not query player device info");
+					}
+					return list;
+				}
+
+				static BOOL CALLBACK DeviceEnumProc(LPGUID lpGUID, LPCWSTR lpszDesc, LPCWSTR lpszDrvName, LPVOID lpContext)
+				{
+					List<DeviceProperty>& list = *((List<DeviceProperty>*)lpContext);
+					DeviceProperty prop;
+					if (lpGUID) {
+						prop.guid = *lpGUID;
+						prop.szGuid = Windows::getStringFromGUID(*lpGUID);
+						prop.name = String::create(lpszDrvName);
+						prop.description = String::create(lpszDesc);
+						list.add_NoLock(prop);
+					}
+					return TRUE;
+				}
+			};
+
 			class AudioPlayerBufferImpl : public AudioPlayerBuffer
 			{
 			public:
-				LPDIRECTSOUND		m_ds;
+				Ref<AudioPlayerImpl> m_player;
 				LPDIRECTSOUNDBUFFER m_dsBuffer;
 				LPDIRECTSOUNDNOTIFY m_dsNotify;
 				HANDLE              m_hNotificationEvents[2];
@@ -66,7 +180,6 @@ namespace slib
 				AudioPlayerBufferImpl()
 				{
 					m_nOffsetNextWrite = m_nNotifySize = 0;
-					m_ds = NULL;
 					m_dsBuffer = NULL;
 					m_dsNotify = NULL;
 					m_flagOpened = sl_true;
@@ -85,107 +198,90 @@ namespace slib
 					LogError("AudioPlayer", text);
 				}
 
-				static Ref<AudioPlayerBufferImpl> create(const AudioPlayerBufferParam& param, const GUID& deviceID)
+				static Ref<AudioPlayerBufferImpl> create(const Ref<AudioPlayerImpl>& player, const AudioPlayerBufferParam& param)
 				{
 					if (param.channelsCount != 1 && param.channelsCount != 2) {
 						return sl_null;
 					}
 					
-					::CoInitializeEx(NULL, COINIT_MULTITHREADED);
+					WAVEFORMATEX wf;
+					wf.wFormatTag = WAVE_FORMAT_PCM;
+					wf.nChannels = param.channelsCount;
+					wf.wBitsPerSample = 16;
+					wf.nSamplesPerSec = param.samplesPerSecond;
+					wf.nBlockAlign = wf.wBitsPerSample * wf.nChannels / 8;
+					wf.nAvgBytesPerSec = wf.nSamplesPerSec * wf.nBlockAlign;
+					wf.cbSize = 0;
 
-					LPDIRECTSOUND ds;
-					HRESULT hr = DirectSoundCreate(&deviceID, &ds, NULL);
+					sl_uint32 samplesPerFrame = wf.nSamplesPerSec * param.frameLengthInMilliseconds / 1000;
+					sl_uint32 sizeBuffer = samplesPerFrame * wf.nBlockAlign * 3;
+					sl_int32 notifySize = sizeBuffer / NUM_PLAY_NOTIFICATIONS;
+
+					DSBUFFERDESC desc;
+					ZeroMemory(&desc, sizeof(DSBUFFERDESC));
+					desc.dwSize = sizeof(DSBUFFERDESC);
+					desc.dwFlags = DSBCAPS_CTRLPOSITIONNOTIFY | DSBCAPS_GETCURRENTPOSITION2 | DSBCAPS_GLOBALFOCUS;
+					desc.dwBufferBytes = sizeBuffer;
+					desc.lpwfxFormat = &wf;
+
+					LPDIRECTSOUNDBUFFER dsBuffer;
+					HANDLE hNotificationEvents[2];
+					hNotificationEvents[0] = CreateEventW(NULL, FALSE, FALSE, NULL);
+					hNotificationEvents[1] = CreateEventW(NULL, FALSE, FALSE, NULL);
+
+					HRESULT hr = player->m_ds->CreateSoundBuffer(&desc, &dsBuffer, NULL);
 					if (SUCCEEDED(hr)) {
-						hr = ds->SetCooperativeLevel(::GetDesktopWindow(), DSSCL_NORMAL);
+						LPDIRECTSOUNDNOTIFY dsNotify;
+						hr = dsBuffer->QueryInterface(IID_IDirectSoundNotify, (VOID**)&dsNotify);
 						if (SUCCEEDED(hr)) {
-							WAVEFORMATEX wf;
-							wf.wFormatTag = WAVE_FORMAT_PCM;
-							wf.nChannels = param.channelsCount;
-							wf.wBitsPerSample = 16;
-							wf.nSamplesPerSec = param.samplesPerSecond;
-							wf.nBlockAlign = wf.wBitsPerSample * wf.nChannels / 8;
-							wf.nAvgBytesPerSec = wf.nSamplesPerSec * wf.nBlockAlign;
-							wf.cbSize = 0;
+							DSBPOSITIONNOTIFY   dsPosNotify[NUM_PLAY_NOTIFICATIONS + 1];
+							sl_int32 notifyIndex = 0;
 
-							sl_uint32 samplesPerFrame = wf.nSamplesPerSec * param.frameLengthInMilliseconds / 1000;
-							sl_uint32 sizeBuffer = samplesPerFrame * wf.nBlockAlign * 3;
-							sl_int32 notifySize = sizeBuffer / NUM_PLAY_NOTIFICATIONS;
+							for (notifyIndex = 0; notifyIndex < NUM_PLAY_NOTIFICATIONS; notifyIndex++) {
+								dsPosNotify[notifyIndex].dwOffset = (notifySize * notifyIndex) + notifySize - 1;
+								dsPosNotify[notifyIndex].hEventNotify = hNotificationEvents[0];
+							}
+							dsPosNotify[notifyIndex].dwOffset = DSBPN_OFFSETSTOP;
+							dsPosNotify[notifyIndex].hEventNotify = hNotificationEvents[1];
 
-							DSBUFFERDESC desc;
-							ZeroMemory(&desc, sizeof(DSBUFFERDESC));
-							desc.dwSize = sizeof(DSBUFFERDESC);
-							desc.dwFlags = DSBCAPS_CTRLPOSITIONNOTIFY | DSBCAPS_GETCURRENTPOSITION2 | DSBCAPS_GLOBALFOCUS;
-							desc.dwBufferBytes = sizeBuffer;
-							desc.lpwfxFormat = &wf;
-
-							LPDIRECTSOUNDBUFFER dsBuffer;
-							HANDLE hNotificationEvents[2];
-							hNotificationEvents[0] = ::CreateEventW(NULL, FALSE, FALSE, NULL);
-							hNotificationEvents[1] = ::CreateEventW(NULL, FALSE, FALSE, NULL);
-
-							hr = ds->CreateSoundBuffer(&desc, &dsBuffer, NULL);
+							hr = dsNotify->SetNotificationPositions(NUM_PLAY_NOTIFICATIONS + 1, dsPosNotify);
 							if (SUCCEEDED(hr)) {
-								LPDIRECTSOUNDNOTIFY dsNotify;
-								hr = dsBuffer->QueryInterface(IID_IDirectSoundNotify, (VOID**)&dsNotify);
+								hr = dsBuffer->Play(0, 0, DSBPLAY_LOOPING);
 								if (SUCCEEDED(hr)) {
-									DSBPOSITIONNOTIFY   dsPosNotify[NUM_PLAY_NOTIFICATIONS + 1];
-									sl_int32 notifyIndex = 0;
-
-									for (notifyIndex = 0; notifyIndex < NUM_PLAY_NOTIFICATIONS; notifyIndex++) {
-										dsPosNotify[notifyIndex].dwOffset = (notifySize * notifyIndex) + notifySize - 1;
-										dsPosNotify[notifyIndex].hEventNotify = hNotificationEvents[0];
-									}
-									dsPosNotify[notifyIndex].dwOffset = DSBPN_OFFSETSTOP;
-									dsPosNotify[notifyIndex].hEventNotify = hNotificationEvents[1];
-
-									hr = dsNotify->SetNotificationPositions(NUM_PLAY_NOTIFICATIONS + 1, dsPosNotify);
-									if (SUCCEEDED(hr)) {
-										hr = dsBuffer->Play(0, 0, DSBPLAY_LOOPING);
-										if (SUCCEEDED(hr)) {
-											Ref<AudioPlayerBufferImpl> ret = new AudioPlayerBufferImpl();
-											if (ret.isNotNull()) {
-												ret->m_ds = ds;
-												ret->m_dsBuffer = dsBuffer;
-												ret->m_dsNotify = dsNotify;
-												ret->m_nNotifySize = notifySize;
-												ret->m_hNotificationEvents[0] = hNotificationEvents[0];
-												ret->m_hNotificationEvents[1] = hNotificationEvents[1];
-												ret->m_nSamplesFrame = samplesPerFrame;
-												ret->m_nBufferSize = sizeBuffer;
+									Ref<AudioPlayerBufferImpl> ret = new AudioPlayerBufferImpl();
+									if (ret.isNotNull()) {
+										ret->m_player = player;
+										ret->m_dsBuffer = dsBuffer;
+										ret->m_dsNotify = dsNotify;
+										ret->m_nNotifySize = notifySize;
+										ret->m_hNotificationEvents[0] = hNotificationEvents[0];
+										ret->m_hNotificationEvents[1] = hNotificationEvents[1];
+										ret->m_nSamplesFrame = samplesPerFrame;
+										ret->m_nBufferSize = sizeBuffer;
 												
-												ret->_init(param);
+										ret->_init(param);
 												
-												if (param.flagAutoStart) {
-													ret->start();
-												}
-												return ret;
-											}
-										} else {
-											logError("Failed to start direct sound looping");
+										if (param.flagAutoStart) {
+											ret->start();
 										}
-									} else {
-										logError("Failed to set dsound notify positions");
+										return ret;
 									}
 								} else {
-									logError("Failed to get dsound notify");
+									logError("Failed to start direct sound looping");
 								}
-								dsBuffer->Release();
 							} else {
-								logError("Failed to create dsound buffer 8");
+								logError("Failed to set dsound notify positions");
 							}
-							ds->Release();
-							CloseHandle(hNotificationEvents[0]);
-							CloseHandle(hNotificationEvents[1]);
 						} else {
-							logError("Direct sound set CooperativeLevel failed.");
+							logError("Failed to get dsound notify");
 						}
+						dsBuffer->Release();
 					} else {
-						if (hr == DSERR_ALLOCATED) {
-							logError("Direct sound playback device is already used");
-						} else {
-							logError("Can not create direct sound playback device");
-						}
+						logError("Failed to create dsound buffer 8");
 					}
+					CloseHandle(hNotificationEvents[0]);
+					CloseHandle(hNotificationEvents[1]);
+					
 					return sl_null;
 				}
 
@@ -203,8 +299,7 @@ namespace slib
 					m_dsNotify->Release();
 					m_dsNotify = NULL;
 
-					m_ds->Release();
-					m_ds = NULL;
+					m_player.setNull();
 
 					for (int i = 0; i < 2; i++) {
 						CloseHandle(m_hNotificationEvents[i]);
@@ -242,7 +337,7 @@ namespace slib
 					}
 					m_flagRunning = sl_false;
 					m_thread->finish();
-					::SetEvent(m_hNotificationEvents[1]);
+					SetEvent(m_hNotificationEvents[1]);
 					m_thread->finishAndWait();
 					m_thread.setNull();
 				}
@@ -254,10 +349,10 @@ namespace slib
 
 				void run()
 				{
-					::CoInitializeEx(NULL, COINIT_MULTITHREADED);
+					CoInitializeEx(NULL, COINIT_MULTITHREADED);
 					Ref<Thread> thread = Thread::getCurrent();
 					while (thread.isNull() || thread->isNotStopping()) {
-						DWORD dwWait = ::WaitForMultipleObjects(2, m_hNotificationEvents, FALSE, INFINITE);
+						DWORD dwWait = WaitForMultipleObjects(2, m_hNotificationEvents, FALSE, INFINITE);
 						if (dwWait == WAIT_OBJECT_0) {
 							onFrame();
 						} else {
@@ -286,86 +381,10 @@ namespace slib
 
 			};
 
-			class AudioPlayerImpl : public AudioPlayer
+			Ref<AudioPlayerBuffer> AudioPlayerImpl::createBuffer(const AudioPlayerBufferParam& param)
 			{
-			public:
-				GUID m_deviceID;
-
-			public:
-				AudioPlayerImpl()
-				{
-				}
-
-				~AudioPlayerImpl()
-				{
-				}
-
-			public:
-				static void logError(String text)
-				{
-					LogError("AudioPlayer", text);
-				}
-
-				static Ref<AudioPlayerImpl> create(const AudioPlayerParam& param)
-				{
-					Ref<AudioPlayerImpl> ret = new AudioPlayerImpl();
-					GUID gid;
-					String deviceID = param.deviceId;
-					if (deviceID.isEmpty()) {
-						gid = DSDEVID_DefaultPlayback;
-					} else {
-						ListLocker<DeviceProperty> props(queryDeviceInfos());
-						sl_size i = 0;
-						for (; i < props.count; i++) {
-							if (deviceID == props[i].szGuid) {
-								gid = props[i].guid;
-								break;
-							}
-						}
-						if (i == props.count) {
-							logError("Failed to find capture device");
-						}
-					}
-					ret->m_deviceID = gid;
-					return ret;
-				}
-
-				Ref<AudioPlayerBuffer> createBuffer(const AudioPlayerBufferParam& param) override
-				{
-					return AudioPlayerBufferImpl::create(param, m_deviceID);
-				}
-
-				struct DeviceProperty {
-					GUID guid;
-					String szGuid;
-					String name;
-					String description;
-				};
-
-				static List<DeviceProperty> queryDeviceInfos()
-				{
-					List<DeviceProperty> list;
-					HRESULT hr = DirectSoundEnumerateW(DeviceEnumProc, (VOID*)&list);
-					if (FAILED(hr)) {
-						logError("Can not query player device info");
-					}
-					return list;
-				}
-
-				static BOOL CALLBACK DeviceEnumProc(LPGUID lpGUID, LPCWSTR lpszDesc, LPCWSTR lpszDrvName, LPVOID lpContext)
-				{
-					List<DeviceProperty>& list = *((List<DeviceProperty>*)lpContext);
-					DeviceProperty prop;
-					if (lpGUID) {
-						prop.guid = *lpGUID;
-						prop.szGuid = Windows::getStringFromGUID(*lpGUID);
-						prop.name = String::create(lpszDrvName);
-						prop.description = String::create(lpszDesc);
-						list.add_NoLock(prop);
-					}
-					return TRUE;
-				}
-			};
+				return AudioPlayerBufferImpl::create(this, param);
+			}
 
 		}
 	}
