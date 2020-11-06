516fb1f38d69ef19a2737dc2e0b76aa9f8f1846b
Fred Zhang <fred1218zh@outlook.com>
2020-11-07 08:55:50 +0900

    [Win32] storage/Fuse: fix compile issue

--- src/slib/storage/fuse.cpp
+++ src/slib/storage/fuse.cpp
@@ -30,6 +30,10 @@
 #define FUSE_USE_VERSION 27
 #include "fuse/fuse.h"
 
+#ifdef SLIB_PLATFORM_IS_WIN32
+typedef fsblkcnt64_t fsblkcnt_t;
+#endif
+
 #include <errno.h>
 #define FUSE_ERROR_CODE(err) (-(int)(err))
 
