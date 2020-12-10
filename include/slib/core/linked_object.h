27daf9a85a251ad88416fc4478b4428f3039879f
pcu <hack_ant@gmail.com>
2020-12-11 08:41:13 +0900

    core: remove .inc files (linked_object.inc, list*.inc)

--- include/slib/core/linked_object.h
+++ include/slib/core/linked_object.h
@@ -64,82 +64,397 @@ namespace slib
 		sl_size m_count;
 
 	public:
-		LinkedObjectList() noexcept;
+		LinkedObjectList() noexcept
+		{
+			m_front.setNull();
+			m_back.setNull();
+			m_count = 0;
+		}
 
-		~LinkedObjectList() noexcept;
+		~LinkedObjectList() noexcept
+		{
+			removeAll_NoLock();
+		}
 
 	public:
-		const Ref<T>& getFront() const noexcept;
+		const Ref<T>& getFront() const noexcept
+		{
+			return m_front;
+		}
 	
-		const Ref<T>& getBack() const noexcept;
+		const Ref<T>& getBack() const noexcept
+		{
+			return m_back;
+		}
 	
-		sl_size getCount() const noexcept;
+		sl_size getCount() const noexcept
+		{
+			return m_count;
+		}
 
-		sl_bool isEmpty() const noexcept;
+		sl_bool isEmpty() const noexcept
+		{
+			return m_front.isNull();
+		}
 	
-		sl_bool isNotEmpty() const noexcept;
+		sl_bool isNotEmpty() const noexcept
+		{
+			return m_front.isNotNull();
+		}
 
-		sl_bool pushBack_NoLock(const Ref<T>& object) noexcept;
+		sl_bool pushBack_NoLock(const Ref<T>& object) noexcept
+		{
+			if (object.isNull()) {
+				return sl_false;
+			}
+			_pushBack(object);
+			return sl_true;
+		}
 
-		sl_bool pushBack(const Ref<T>& object) noexcept;
+		sl_bool pushBack(const Ref<T>& object) noexcept
+		{
+			if (object.isNull()) {
+				return sl_false;
+			}
+			ObjectLocker lock(this);
+			_pushBack(object);
+			return sl_true;
+		}
 
-		sl_bool popBack_NoLock(Ref<T>* _out = sl_null) noexcept;
+		sl_bool popBack_NoLock(Ref<T>* _out = sl_null) noexcept
+		{
+			Ref<T> old = _popBack();
+			if (old.isNotNull()) {
+				if (_out) {
+					*_out = Move(old);
+				}
+				return sl_true;
+			} else {
+				return sl_false;
+			}
+		}
 
-		sl_bool popBack(Ref<T>* _out = sl_null) noexcept;
+		sl_bool popBack(Ref<T>* _out = sl_null) noexcept
+		{
+			Ref<T> old;
+			{
+				ObjectLocker lock(this);
+				old = _popBack();
+			}
+			if (old.isNotNull()) {
+				if (_out) {
+					*_out = Move(old);
+				}
+				return sl_true;
+			} else {
+				return sl_false;
+			}
+		}
 
-		sl_bool pushFront_NoLock(const Ref<T>& object) noexcept;
+		sl_bool pushFront_NoLock(const Ref<T>& object) noexcept
+		{
+			if (object.isNull()) {
+				return sl_false;
+			}
+			_pushFront(object);
+			return sl_true;
+		}
 
-		sl_bool pushFront(const Ref<T>& object) noexcept;
+		sl_bool pushFront(const Ref<T>& object) noexcept
+		{
+			if (object.isNull()) {
+				return sl_false;
+			}
+			ObjectLocker lock(this);
+			_pushFront(object);
+			return sl_true;
+		}
 
-		sl_bool popFront_NoLock(Ref<T>* _out = sl_null) noexcept;
+		sl_bool popFront_NoLock(Ref<T>* _out = sl_null) noexcept
+		{
+			Ref<T> old = _popFront();
+			if (old.isNotNull()) {
+				if (_out) {
+					*_out = Move(old);
+				}
+				return sl_true;
+			} else {
+				return sl_false;
+			}
+		}
 
-		sl_bool popFront(Ref<T>* _out = sl_null) noexcept;
+		sl_bool popFront(Ref<T>* _out = sl_null) noexcept
+		{
+			Ref<T> old;
+			{
+				ObjectLocker lock(this);
+				old = _popFront();
+			}
+			if (old.isNotNull()) {
+				if (_out) {
+					*_out = Move(old);
+				}
+				return sl_true;
+			} else {
+				return sl_false;
+			}
+		}
 
 		/* unsynchronized function */
-		sl_bool insertBefore(const Ref<T>& objectWhere, const Ref<T>& objectNew) noexcept;
+		sl_bool insertBefore(const Ref<T>& objectWhere, const Ref<T>& objectNew) noexcept
+		{
+			if (objectWhere.isNotNull()) {
+				_insertBefore(objectWhere, objectNew);
+				return sl_true;
+			} else {
+				return pushFront_NoLock(objectNew);
+			}
+		}
 		
 		/* unsynchronized function */
-		sl_bool insertAfter(const Ref<T>& objectWhere, const Ref<T>& objectNew) noexcept;
+		sl_bool insertAfter(const Ref<T>& objectWhere, const Ref<T>& objectNew) noexcept
+		{
+			if (objectWhere.isNotNull()) {
+				_insertAfter(objectWhere, objectNew);
+				return sl_true;
+			} else {
+				return pushBack_NoLock(objectNew);
+			}
+		}
 
-		sl_bool remove_NoLock(const Ref<T>& object) noexcept;
+		sl_bool remove_NoLock(const Ref<T>& object) noexcept
+		{
+			if (object.isNull()) {
+				return sl_false;
+			}
+			_remove(object);
+			return sl_true;
+		}
 
-		sl_bool remove(const Ref<T>& object) noexcept;
+		sl_bool remove(const Ref<T>& object) noexcept
+		{
+			ObjectLocker lock(this);
+			return remove_NoLock(object);
+		}
 
-		sl_size removeAll_NoLock() noexcept;
+		sl_size removeAll_NoLock() noexcept
+		{
+			sl_size count = m_count;
+			_init();
+			return count;
+		}
 		
-		sl_size removeAll() noexcept;
+		sl_size removeAll() noexcept
+		{
+			Ref<T> front;
+			sl_size count;
+			{
+				ObjectLocker lock(this);
+				front = Move(m_front);
+				count = m_count;
+				_init();
+			}
+			return count;
+		}
 
-		void merge(LinkedObjectList<T>* other) noexcept;
+		void merge(LinkedObjectList<T>* other) noexcept
+		{
+			if ((void*)this == (void*)other) {
+				return;
+			}
+			MultipleObjectsLocker lock(this, other);
+			T* _front = other->m_front.get();
+			if (_front) {
+				sl_size countNew = m_count + other->getCount();
+				if (m_back.isNotNull()) {
+					m_back->next = Move(other->m_front);
+					_front->before = Move(m_back);
+				} else {
+					m_front = Move(other->m_front);
+				}
+				m_back = Move(other->m_back);
+				other->_init();
+				m_count = countNew;
+			}
+		}
 
-		Array< Ref<T> > toArray_NoLock() const noexcept;
+		Array< Ref<T> > toArray_NoLock() const noexcept
+		{
+			Array< Ref<T> > ret;
+			if (m_count) {
+				ret = Array< Ref<T> >::create(m_count);
+				if (ret.isNotNull()) {
+					Ref<T>* buf = ret.getData();
+					sl_size index = 0;
+					T* now = m_front.get();
+					while (now) {
+						buf[index] = now;
+						now = now->next.get();
+						index++;
+					}
+				}
+			}
+			return ret;
+		}
 
-		Array< Ref<T> > toArray() const noexcept;
+		Array< Ref<T> > toArray() const noexcept
+		{
+			ObjectLocker lock(this);
+			return toArray_NoLock();
+		}
 
-		List< Ref<T> > toList_NoLock() const noexcept;
+		List< Ref<T> > toList_NoLock() const noexcept
+		{
+			List< Ref<T> > ret;
+			if (m_count) {
+				ret = List< Ref<T> >::create(m_count);
+				if (ret.isNotNull()) {
+					ListElements< Ref<T> > list(ret);
+					sl_size index = 0;
+					T* now = m_front.get();
+					while (now) {
+						list[index] = now;
+						now = now->next.get();
+						index++;
+					}
+				}
+			}
+			return ret;
+		}
 
-		List< Ref<T> > toList() const noexcept;
+		List< Ref<T> > toList() const noexcept
+		{
+			ObjectLocker lock(this);
+			return toList_NoLock();
+		}
 
 	protected:
-		void _init() noexcept;
+		void _init() noexcept
+		{
+			m_front.setNull();
+			m_back.setNull();
+			m_count = 0;
+		}
 
-		void _pushBack(const Ref<T>& object) noexcept;
+		void _pushBack(const Ref<T>& object) noexcept
+		{
+			if (m_back.isNotNull()) {
+				m_back->next = object;
+				object->before = Move(m_back);
+				object->next.setNull();
+				m_back = object;
+			} else {
+				m_front = object;
+				m_back = object;
+			}
+			m_count++;
+		}
 
-		Ref<T> _popBack() noexcept;
+		Ref<T> _popBack() noexcept
+		{
+			Ref<T> back = Move(m_back);
+			if (back.isNotNull()) {
+				m_count--;
+				Ref<T>& before = back->before;
+				if (before.isNotNull()) {
+					before->next.setNull();
+					m_back = Move(before);
+				} else {
+					m_front.setNull();
+					m_back.setNull();
+				}
+				back->next.setNull();
+			}
+			return back;
+		}
 
-		void _pushFront(const Ref<T>& object) noexcept;
+		void _pushFront(const Ref<T>& object) noexcept
+		{
+			if (m_front.isNotNull()) {
+				m_front->before = object;
+				object->next = Move(m_front);
+				object->before.setNull();
+				m_front = object;
+			} else {
+				m_front = object;
+				m_back = object;
+			}
+			m_count++;
+		}
 
-		Ref<T> _popFront() noexcept;
+		Ref<T> _popFront() noexcept
+		{
+			Ref<T> front = Move(m_front);
+			if (front.isNotNull()) {
+				m_count--;
+				Ref<T>& next = front->next;
+				if (next.isNotNull()) {
+					next->before.setNull();
+					m_front = Move(next);
+				} else {
+					m_front.setNull();
+					m_back.setNull();
+				}
+				front->before.setNull();
+			}
+			return front;
+		}
 
-		void _remove(const Ref<T>& object) noexcept;
+		void _remove(const Ref<T>& object) noexcept
+		{
+			T* before = object->before.get();
+			T* next = object->next.get();
+			if (m_front == object) {
+				m_front = Move(object->next);
+			} else if (before) {
+				before->next = Move(object->next);
+			} else {
+				return;
+			}
+			if (m_back == object) {
+				m_back = Move(object->before);
+			} else if (next) {
+				next->before = Move(object->before);
+			} else {
+				return;
+			}
+			m_count--;
+		}
 
-		void _insertBefore(const Ref<T>& objectWhere, const Ref<T>& objectNew) noexcept;
+		void _insertBefore(const Ref<T>& objectWhere, const Ref<T>& objectNew) noexcept
+		{
+			objectNew->next = objectWhere;
+			T* before = objectWhere->before.get();
+			objectNew->before = Move(objectWhere->before);
+			objectWhere->before = objectNew;
+			if (m_front == objectWhere) {
+				m_front = objectNew;
+			} else if (before) {
+				before->next = objectNew;
+			} else {
+				return;
+			}
+			m_count++;
+		}
 
-		void _insertAfter(const Ref<T>& objectWhere, const Ref<T>& objectNew) noexcept;
+		void _insertAfter(const Ref<T>& objectWhere, const Ref<T>& objectNew) noexcept
+		{
+			objectNew->before = objectWhere;
+			T* next = objectWhere->next.get();
+			objectNew->next = Move(objectWhere->next);
+			objectWhere->next = objectNew;
+			if (m_back == objectWhere) {
+				m_back = objectNew;
+			} else if (next) {
+				next->before = objectNew;
+			} else {
+				return;
+			}
+			m_count++;
+		}
 	
 	};
 
 }
-
-#include "detail/linked_object.inc"
 
 #endif
