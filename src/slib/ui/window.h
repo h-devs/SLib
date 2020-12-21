58945e65d73f3c58f18049aba0f047ce0f607d3b
Fred Zhang <fred1218zh@outlook.com>
2020-12-21 16:24:33 +0900

    ui/Window: added source private header

---
+++ src/slib/ui/window.h
@@ -0,0 +1,59 @@
+#ifndef CHECKHEADER_SLIB_SRC_UI_WINDOW
+#define CHECKHEADER_SLIB_SRC_UI_WINDOW
+
+#include "slib/ui/window.h"
+
+#include "slib/ui/core.h"
+#include "slib/ui/screen.h"
+#include "slib/ui/platform.h"
+
+namespace slib
+{
+	namespace priv
+	{
+		namespace window
+		{
+
+			class WindowHelper : public Window
+			{
+			public:
+				UIRect makeFrame(const UIRect& _frame)
+				{
+					UIRect frameScreen;
+					Ref<Screen> screen = m_screen;
+					if (screen.isNotNull()) {
+						frameScreen = screen->getRegion();
+					} else {
+						frameScreen = UI::getScreenRegion();
+					}
+					UIRect frame;
+					if (m_flagFullScreen) {
+						frame.setLeftTop(0, 0);
+						frame.setSize(frameScreen.getSize());
+					} else {
+						frame = _frame;
+						if (m_flagCenterScreen) {
+							frame.setLocation(frameScreen.getWidth() / 2 - frame.getWidth() / 2, frameScreen.getHeight() / 2 - frame.getHeight() / 2);
+						}
+					}
+					frame.fixSizeError();
+					return frame;
+				}
+
+			};
+
+			static UIRect MakeWindowFrame(Window* window)
+			{
+				return ((WindowHelper*)window)->makeFrame(window->getFrame());
+			}
+
+			static UIRect MakeWindowFrame(Window* window, const UIRect& frame)
+			{
+				return ((WindowHelper*)window)->makeFrame(frame);
+			}
+
+		}
+	}
+}
+
+#endif
