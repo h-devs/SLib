78044df1abf29d261cc30b7745dbfed5bc89ba94
Fred Zhang <fredzhang1218@outlook.com>
2021-05-05 22:23:39 +0900

    network: support Json conversion for IP, Socket and Mac addresses

--- include/slib/network/json_conv.h
+++ include/slib/network/json_conv.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2008-2018 SLIBIO <https://github.com/SLIBIO>
+ *   Copyright (c) 2008-2021 SLIBIO <https://github.com/SLIBIO>
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
@@ -20,42 +20,33 @@
  *   THE SOFTWARE.
  */
 
-#ifndef CHECKHEADER_SLIB_NETWORK_HEADER
-#define CHECKHEADER_SLIB_NETWORK_HEADER
+#ifndef CHECKHEADER_SLIB_NETWORK_JSON_CONV
+#define CHECKHEADER_SLIB_NETWORK_JSON_CONV
 
-#include "network/constants.h"
+#include "mac_address.h"
+#include "ip_address.h"
+#include "socket_address.h"
 
-#include "network/mac_address.h"
-#include "network/ip_address.h"
-#include "network/socket_address.h"
-#include "network/json_conv.h"
+#include "../core/json.h"
 
-#include "network/os.h"
-#include "network/socket.h"
-#include "network/async.h"
-#include "network/io.h"
-#include "network/event.h"
+namespace slib
+{
 
-#include "network/tcpip.h"
-#include "network/tcpip_fragment.h"
-#include "network/dns.h"
-#include "network/icmp.h"
-#include "network/nat.h"
-#include "network/ethernet.h"
-#include "network/arp.h"
+	void FromJson(const Json& json, MacAddress& _out);
+	void ToJson(Json& json, const MacAddress& _in);
 
-#include "network/url.h"
-#include "network/url_request.h"
-#include "network/curl.h"
-#include "network/http.h"
-#include "network/stun.h"
+	void FromJson(const Json& json, IPv4Address& _out);
+	void ToJson(Json& json, const IPv4Address& _in);
 
-#include "network/capture.h"
-#include "network/pcap.h"
-#include "network/npcap.h"
-#include "network/tap.h"
+	void FromJson(const Json& json, IPv6Address& _out);
+	void ToJson(Json& json, const IPv6Address& _in);
 
-#include "network/pseudo_tcp.h"
-#include "network/pseudo_tcp_message.h"
+	void FromJson(const Json& json, IPAddress& _out);
+	void ToJson(Json& json, const IPAddress& _in);
+
+	void FromJson(const Json& json, SocketAddress& _out);
+	void ToJson(Json& json, const SocketAddress& _in);
+
+}
 
 #endif
