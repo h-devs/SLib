ceddc2d80654f9cce9e81711b45ea05a6b8afb06
Fred Zhang <fred1218zh@outlook.com>
2021-05-19 15:52:54 +0900

    minor update

--- include/slib/device/device.h
+++ include/slib/device/device.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2008-2018 SLIBIO <https://github.com/SLIBIO>
+ *   Copyright (c) 2008-2021 SLIBIO <https://github.com/SLIBIO>
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
