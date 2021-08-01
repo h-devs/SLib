4fdd4c60047d6e0351ea3fb097cfbf6d22e8fea6
Fred Zhang <fredzhang1218@outlook.com>
2021-08-02 03:21:16 +0900

    tool/BinaryToArray: added new commands

--- tool/src/BinaryToArray/main.cpp
+++ tool/src/BinaryToArray/main.cpp
@@ -27,50 +27,147 @@ using namespace slib;
 int main(int argc, const char * argv[])
 {
 	if (argc < 3) {
+		Println("Usage: BinaryToArray [compress|decompress|compress-zstd|decompress-zstd|d] <source-path> <output-path>");
 		return -1;
 	}
 	String fileSource;
 	String fileOutput;
-	sl_bool flagCompress;
+	sl_bool flagToBinary = sl_false;
+	sl_bool flagCompressZstd = sl_false;
+	sl_bool flagCompressZlib = sl_false;
 	if (Base::equalsString(argv[1], "compress")) {
+		if (argc != 4) {
+			Println("Invalid argument count!");
+			return -1;
+		}
+		flagCompressZlib = sl_true;
+		fileSource = argv[2];
+		fileOutput = argv[3];
+	} else if (Base::equalsString(argv[1], "compress-zstd")) {
+		if (argc != 4) {
+			Println("Invalid argument count!");
+			return -1;
+		}
+		flagCompressZstd = sl_true;
+		fileSource = argv[2];
+		fileOutput = argv[3];
+	} else if (Base::equalsString(argv[1], "decompress")) {
 		if (argc != 4) {
 			return -1;
 		}
-		flagCompress = sl_true;
+		flagCompressZlib = sl_true;
+		flagToBinary = sl_true;
+		fileSource = argv[2];
+		fileOutput = argv[3];
+	} else if (Base::equalsString(argv[1], "decompress-zstd")) {
+		if (argc != 4) {
+			Println("Invalid argument count!");
+			return -1;
+		}
+		flagCompressZstd = sl_true;
+		flagToBinary = sl_true;
+		fileSource = argv[2];
+		fileOutput = argv[3];
+	} else if (Base::equalsString(argv[1], "d")) {
+		if (argc != 4) {
+			Println("Invalid argument count!");
+			return -1;
+		}
+		flagToBinary = sl_true;
 		fileSource = argv[2];
 		fileOutput = argv[3];
 	} else {
 		if (argc != 3) {
+			Println("Invalid argument count!");
 			return -1;
 		}
-		flagCompress = sl_false;
 		fileSource = argv[1];
 		fileOutput = argv[2];
 	}
-	Memory mem = File::readAllBytes(fileSource);
-	if (mem.isNull()) {
-		return -1;
-	}
-	if (flagCompress) {
-		mem = Zstd::compress(mem.getData(), mem.getSize(), 22);
-		if (mem.isNull()) {
+	if (flagToBinary) {
+		String text = File::readAllText(fileSource);
+		if (text.isEmpty()) {
+			Println("Source content is empty!");
 			return -1;
 		}
-	}
-	StringBuffer sb;
-	sl_size n = mem.getSize();
-	sl_uint8* data = (sl_uint8*)(mem.getData());
-	for (sl_size i = 0; i < n; i++) {
-		if (i == n - 1) {
-			sb.add(String::format("%d", data[i]));
-		} else if ((i & 15) != 15) {
-			sb.add(String::format("%d,", data[i]));
-		} else {
-			sb.add(String::format("%d,\r\n", data[i]));
+		CList<sl_uint8> buf;
+		sl_size len = text.getLength();
+		char* str = text.getData();
+		sl_uint32 n = 0;
+		for (sl_size i = 0; i < len; i++) {
+			char ch = str[i];
+			if (ch == ',') {
+				buf.add_NoLock(n);
+				n = 0;
+			} else if (SLIB_CHAR_IS_DIGIT(ch)) {
+				n = (sl_uint32)(n * 10 + (sl_uint32)(ch - '0'));
+				if (n >> 8) {
+					Println("Invalid number (bigger than 255)!");
+					return -1;
+				}
+			} else if (!SLIB_CHAR_IS_WHITE_SPACE(ch)) {
+				Println("Invalid character: %c", ch);
+				return -1;
+			}
 		}
-	}
-	if (File::writeAllBytes(fileOutput, sb.mergeToMemory()) == 0) {
-		return -1;
+		buf.add_NoLock(n);
+		Memory mem;
+		if (flagCompressZlib) {
+			mem = Zlib::decompress(buf.getData(), buf.getCount());
+			if (mem.isNull()) {
+				Println("Failed to decompress!");
+				return -1;
+			}
+		} else if (flagCompressZstd) {
+			mem = Zstd::decompress(buf.getData(), buf.getCount());
+			if (mem.isNull()) {
+				Println("Failed to decompress!");
+				return -1;
+			}
+		} else {
+			mem = Memory::createStatic(buf.getData(), buf.getCount());
+		}
+		if (File::writeAllBytes(fileOutput, mem) != mem.getSize()) {
+			Println("Failed to write!");
+			return -1;
+		}
+	} else {
+		Memory mem = File::readAllBytes(fileSource);
+		if (mem.isNull()) {
+			Println("Source content is empty!");
+			return -1;
+		}
+		if (flagCompressZlib) {
+			mem = Zlib::compress(mem.getData(), mem.getSize());
+			if (mem.isNull()) {
+				Println("Failed to compress!");
+				return -1;
+			}
+		} else if (flagCompressZstd) {
+			mem = Zstd::compress(mem.getData(), mem.getSize(), 22);
+			if (mem.isNull()) {
+				Println("Failed to compress!");
+				return -1;
+			}
+		}
+		StringBuffer sb;
+		sl_size n = mem.getSize();
+		sl_uint8* data = (sl_uint8*)(mem.getData());
+		for (sl_size i = 0; i < n; i++) {
+			sb.add(String::fromUint32(data[i]));
+			if (i != n - 1) {
+				if ((i & 15) == 15) {
+					sb.addStatic(",\r\n");
+				} else {
+					sb.addStatic(",");
+				}
+			}
+		}
+		mem = sb.mergeToMemory();
+		if (File::writeAllBytes(fileOutput, mem) != mem.getSize()) {
+			Println("Failed to write!");
+			return -1;
+		}
 	}
 	return 0;
 }
