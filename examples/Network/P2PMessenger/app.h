78b1de48a9f167ddd0bbf636d305c71ff06f393c
Fred Zhang <fredzhang1218@outlook.com>
2022-01-20 18:52:21 +0900

    examples/Network: added `P2PMessenger` example

---
+++ examples/Network/P2PMessenger/app.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <slib.h>
+
+using namespace slib;
+
+class P2PMsgApp : public UIApp
+{
+	SLIB_APPLICATION(P2PMsgApp)
+public:
+	P2PMsgApp();
+	
+protected:
+	void onStart() override;
+
+	void onExit() override;
+
+private:
+	void addToBoard(const StringParam& title, const StringParam& content);
+	
+protected:
+	Ref<P2PSocket> m_p2p;
+	Ref<Timer> m_timerBroadcast;
+
+	Ref<LabelList> m_lstNames;
+	Ref<EditView> m_txtBoard;
+	Ref<EditView> m_txtInput;
+
+	CHashMap<P2PNodeId, String> m_nodeNames;
+
+};
