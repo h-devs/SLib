a01d2e305f2ed6253e3fe5617976331e93c188d9
Fred Zhang <fred1218zh@outlook.com>
2022-05-24 17:56:35 -0700

    external/FreeType: removed build files

--- external/build/CMake/external.cmake
+++ external/build/CMake/external.cmake
@@ -42,7 +42,6 @@ if (ANDROID AND SLIB_X86)
 endif ()
 
 include ("${CMAKE_CURRENT_LIST_DIR}/external/yasm.cmake")
-include ("${CMAKE_CURRENT_LIST_DIR}/external/freetype.cmake")
 include ("${CMAKE_CURRENT_LIST_DIR}/external/sqlite.cmake")
 include ("${CMAKE_CURRENT_LIST_DIR}/external/opus.cmake")
 include ("${CMAKE_CURRENT_LIST_DIR}/external/vpx.cmake")
