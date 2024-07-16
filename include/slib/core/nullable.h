b2b90388f230b1e4c629db52c18974159b4f4be6
Fred Zhang <fredzhang1218@outlook.com>
2024-07-16 15:18:18 +0900

    minor update

--- include/slib/core/nullable.h
+++ include/slib/core/nullable.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2008-2018 SLIBIO <https://github.com/SLIBIO>
+ *   Copyright (c) 2008-2024 SLIBIO <https://github.com/SLIBIO>
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
@@ -45,7 +45,7 @@ namespace slib
 		Nullable(const Nullable<OTHER>& other): flagNull(other.flagNull), flagUndefined(other.flagUndefined), value(other.value) {}
 
 		template <class... ARGS>
-		Nullable(ARGS... args): flagNull(sl_false), flagUndefined(sl_false), value(Forward<ARGS...>(args...)) {}
+		Nullable(ARGS&&... args): flagNull(sl_false), flagUndefined(sl_false), value(Forward<ARGS...>(args...)) {}
 
 	public:
 		operator T const&() const noexcept
